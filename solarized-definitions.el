9a8e6553c787a549b3c52c79eae653ff8a2712a5
Mads Hartmann <mads379@gmail.com>
2011-05-03 21:55:19 +0200

    Styling of linum so it looks as you would expect. Works best with (setq linum-format %4d) IMO

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -97,6 +97,7 @@ will use the 256 degraded color mode."
           ((t (:foreground ,base0 :background ,base03 :inverse-video t))))
          (escape-glyph-face ((t (:foreground ,red))))
          (fringe ((t (:foreground ,base01 :background ,base02))))
+         (linum ((t (:foreground ,base01 :background ,base02))))
          (header-line ((t (:foreground ,base0 :background ,base2))))
          (highlight ((t (:background ,base02))))
          (hl-line ((t (:background ,base02))))
