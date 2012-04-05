348a923c559f2c1bff0ad6eb2b0ff1f971fb37d6
Greg Pfeil <greg@technomadic.org>
2012-04-04 19:04:25 -0600

    Don't let hl-line stomp on formatting.

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -187,7 +187,7 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
              (linum ((t (,@fg-base01 ,@bg-base02))))
              (header-line ((t (,@fg-base0 ,@bg-base02 ,@fmt-revbb)))) ; Pmenu
              (highlight ((t (,@bg-base02))))
-             (hl-line ((t (,@fmt-uopt ,@bg-base02)))) ; CursorLine
+             (hl-line ((t (:underline ,opt-under ,@bg-base02)))) ; CursorLine
              (isearch ((t (,@fmt-stnd ,@fg-orange ,@bg-back)))) ; IncSearch
              (isearch-fail ((t (,@fmt-stnd ,@fg-orange ,@bg-back)))) ; IncSearch
              (lazy-highlight ((t (,@fmt-revr ,@fg-yellow ,@bg-back)))) ; Search
