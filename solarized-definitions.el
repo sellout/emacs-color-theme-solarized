6a7b7d4f43e20e3036328531079e9f07f7a565b5
Greg Pfeil <greg@technomadic.org>
2012-04-09 07:29:50 -0600

    Bring Flymake highlighting in line with VIM definitions.
    Fixes issue #52.

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -341,8 +341,8 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
              ;; extra modules
              ;; -------------
              ;; Flymake
-             (flymake-errline ((t (,@bg-base3))))
-             (flymake-warnline ((t (,@bg-base02))))
+             (flymake-errline ((t (,@fmt-revr ,@fg-red)))) ; ErrorMsg
+             (flymake-warnline ((t (,@fmt-bold ,@fg-red)))) ; WarningMsg
              ;; column-marker
              (column-marker-1 ((t (,@bg-base01))))
              (column-marker-2 ((t (,@bg-cyan))))
