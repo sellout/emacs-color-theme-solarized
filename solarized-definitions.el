1aba0ed693bdc8a302fe310ba8218a5e4b162958
Greg Pfeil <greg@technomadic.org>
2012-04-09 16:33:00 -0600

    Override the default background colors for Flymake.
    Fixes issue #52.

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -341,8 +341,9 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
              ;; extra modules
              ;; -------------
              ;; Flymake
-             (flymake-errline ((t (,@fmt-revr ,@fg-red)))) ; ErrorMsg
-             (flymake-warnline ((t (,@fmt-bold ,@fg-red)))) ; WarningMsg
+             (flymake-errline ((t (,@fmt-revr ,@fg-red ,@bg-back)))) ; ErrorMsg
+             (flymake-warnline ; WarningMsg
+              ((t (,@fmt-bold ,@fg-red ,@bg-back))))
              ;; column-marker
              (column-marker-1 ((t (,@bg-base01))))
              (column-marker-2 ((t (,@bg-cyan))))
