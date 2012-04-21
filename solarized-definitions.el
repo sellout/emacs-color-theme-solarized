e268fa4b4ae23d5510c4ff5cf9fb31bd67ec0238
Antoine Beaupré <anarcat@koumbit.org>
2012-04-21 11:32:05 -0400

    fix modeline background

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -247,9 +247,11 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
                 (menu (,@fg-base0 ,@bg-base02))
                 (minibuffer-prompt (,@fmt-bold ,@fg-cyan)) ; Question
                 (mode-line ; StatusLine
-                 (,@fg-base1 ,@bg-base02 ,@fmt-revbb :box nil))
+                 (,@fg-base03 ,@bg-base1 ,@fmt-revbb :box nil))
                 (mode-line-inactive    ; StatusLineNC
-                 (,@fg-base00 ,@bg-base02 ,@fmt-revbb :box nil))
+                 (,@fg-base03 ,@bg-base00 ,@fmt-revbb :box nil))
+                (mode-line-buffer-id
+                 ((t (,@fg-base03))))
                 (region (,@fg-base01 ,@bg-base03 ,@fmt-revbb)) ; Visual
                 (secondary-selection (,@bg-base02))
                 (shadow (,@fg-base01))
