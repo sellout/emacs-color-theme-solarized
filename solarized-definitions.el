sellout/emacs-color-theme-solarized

commit daff304468efeab8ad32da338fcdc063b909a89b

Use cl-flet instead of flet in Emacs version >= 24.3

solarized-definitions.el
@@ -76,8 +76,15 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
    column is a different set, one of which will be chosen based on term
    capabilities, etc.")
 
+(defvar which-flet
+  "This variable will store either flet or cl-flet depending on the Emacs
+  version. flet was deprecated in in 24.3")
+(if (and (> emacs-major-version 24) (> emacs-minor-version 2))
+    (fset 'which-flet 'cl-flet)
+  (fset 'which-flet 'flet))
+
 (defun solarized-color-definitions (mode)
-  (flet ((find-color (name)
+  (which-flet ((find-color (name)
            (let* ((index (if window-system
                              (if solarized-degrade
                                  3
@@ -143,7 +150,7 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
               (bg-violet `(:background ,violet))
               (bg-blue `(:background ,blue))
               (bg-cyan `(:background ,cyan))
-              
+
               (fg-base03 `(:foreground ,base03))
               (fg-base02 `(:foreground ,base02))
               (fg-base01 `(:foreground ,base01))

commit b2262b2703b8aa3bf6dfa93498e769125261333c

solarized-definitions.el: fix comparison error in which-flet fset logic.
flet was deprecated in 24.3, but daff304 accidentally used > 25.2 instead
of >= 24.3 as a condition. This commit fixes that.

solarized-definitions.el
@@ -79,7 +79,8 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
 (defvar which-flet
   "This variable will store either flet or cl-flet depending on the Emacs
   version. flet was deprecated in in 24.3")
-(if (and (> emacs-major-version 24) (> emacs-minor-version 2))
+(if (or (> emacs-major-version 24)
+        (and (>= emacs-major-version 24) (> emacs-minor-version 2)))
     (fset 'which-flet 'cl-flet)
   (fset 'which-flet 'flet))
 