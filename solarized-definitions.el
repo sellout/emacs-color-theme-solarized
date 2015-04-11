903b99ee42d840be2af35ac2409b8a2b6a7cd9a6
Antoine Beaupré <anarcat@koumbit.org>
2015-04-11 11:38:31 -0400

    don't hardcode buffer-id
    i had my own customization elsewhere, removing it made it fine and nice

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -247,11 +247,9 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
                 (menu (,@fg-base0 ,@bg-base02))
                 (minibuffer-prompt (,@fmt-bold ,@fg-cyan)) ; Question
                 (mode-line ; StatusLine
-                 (,@fg-base03 ,@bg-base1 ,@fmt-revbb :box nil))
+                 (,@fg-base03 ,@bg-base01 ,@fmt-revbb :box nil))
                 (mode-line-inactive    ; StatusLineNC
-                 (,@fg-base03 ,@bg-base00 ,@fmt-revbb :box nil))
-                (mode-line-buffer-id
-                 ((t (,@fg-base03))))
+                 (,@fg-base03 ,@bg-base02 ,@fmt-revbb :box nil))
                 (region (,@fg-base01 ,@bg-base03 ,@fmt-revbb)) ; Visual
                 (secondary-selection (,@bg-base02))
                 (shadow (,@fg-base01))
