30dfe48d811fbbe7180bda3b98bf10e4037b6061
Greg Pfeil <greg@technomadic.org>
2023-01-31 19:58:32 -0700

    Remove color-theme support
    Fixes #213.

--- color-theme-solarized-pkg.el
+++ color-theme-solarized-pkg.el
@@ -1,1 +1,1 @@
-(define-package "color-theme-solarized" "%%version%%" "Solarized themes for Emacs" '((color-theme "6.5.5")))
+(define-package "color-theme-solarized" "%%version%%" "Solarized theme for Emacs")
